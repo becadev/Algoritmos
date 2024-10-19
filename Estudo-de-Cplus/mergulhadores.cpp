49909d1171986c21ef7d294e7d0221bb9711042d
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-19 20:42:01 +0000

    questão mergulhadores

--- Estudo-de-Cplus/mergulhadores.cpp
+++ Estudo-de-Cplus/mergulhadores.cpp
@@ -1,35 +1,45 @@
 # include <iostream>
 # include <vector>
-#include <algorithm>
 using namespace std;
-
+//  Depois refazer de uma forma mais eficiente
 int main(){
-    int qtd, vivos;
-    std::cin >> qtd >> vivos;
-    vector<int>total(qtd);
-    vector<int>voltaram(qtd);
-    for (int i = 0 ; i < qtd ; i++){
-        std::cin >> voltaram[i];
-    }
-    sort(voltaram.begin(), voltaram.end());
-    for (int i=0; i < qtd ; i++){
-        total[i]=i;
-    }
+        int qtd, vivos,cont=0;
+        std::cin >> qtd >> vivos;
 
-    for (int j = 0 ; j < vivos ; j++){
-        for (int k = 0 ; k < qtd ; k++){
-            if (voltaram[j]==total[k]){
-                total.erase (total.begin()+ k);
+        vector<int>total(qtd); // referente ao total de mergulhadores que foram
+        vector<int>voltaram(vivos); // referente ao total de mergulhaores que voltaram
+
+        //  Recebe o id dos mergulhadores que voltaram
+        for (int i = 0 ; i < vivos ; i++){
+            std::cin >> voltaram[i];
         }
-    }
-    if  ((sizeof total/sizeof total[0])>=1){
-        for (int i = 0 ; i<sizeof total/sizeof total[0]; i++){
-            std::cout << total[i] << " ";
+        // Faz um array de 1 até qtd
+        for (int i=0; i < qtd ; i++){
+            total[i] = i+1;
         }
-    }else{
-        std::cout << "*" << std::endl;
-    }
-    }
+        // Verifica quem não voltou
+        for (int j = 0 ; j < vivos ; j++){
+            for (int k = 0 ; k < qtd ; k++){
+                if (voltaram[j] == total[k]){
+                    total[k] = 0;
+                }
+            }  
+        }
+        // Mostra quem não voltou ou se voltou todos
+        
+        for (int i = 0 ; i < qtd ; i++){
+            cont+=total[i];       
+        }
+        if (cont >=1){
+            for (int i = 0 ; i<qtd; i++){
+                if (total[i] == 0){
+                    continue;
+                }else{
+                    std::cout << total[i] << " "; 
+                }
+            } 
+        }else{
+            std::cout << "*" << std::endl; 
+        } 
+        return 0;
 }
-
-// sizeof p / sizeof p[0]
