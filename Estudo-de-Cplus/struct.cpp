96a34f8c3cfcd5a4d8152d2d634e9f0752035654
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-22 01:17:04 +0000

    estudo de struct

---
+++ Estudo-de-Cplus/struct.cpp
@@ -0,0 +1,14 @@
+
+
+struct fracao{  // Class, agrupa diferentes dados relacionados em um único tipo.
+    int numerador;
+    int denominador;
+    };
+        fracao soma(fracao &f1, fracao &f2){    // Atributo
+        // TODO
+        }
+    int main(){
+        fracao f1, f2; // declarando
+        // TODO
+    return 0;
+}
