ba6f48cbcc928789f39931db0fbca3b99af5c784
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-02-10 17:27:59 +0000

    falta testar

--- Ordenacao/ordenacao.cpp
+++ Ordenacao/ordenacao.cpp
@@ -1,7 +1,7 @@
 #include "ordenacao.hpp"
 
 /* Big(O) ==> O(n) */
-bool ordenado(int a[],  unsigned int t){
+bool ordenado(int a[],  unsigned int t){ // verificar se já não está ordenado
     int i, ordenado = true;
     for (i = 1; i < t; ++i)
         if (a[i - 1] > a[i]) {
@@ -11,45 +11,67 @@ bool ordenado(int a[],  unsigned int t){
     return ordenado;
 }
 
-/* Big(o) ==> O()*/
-void selecao(int a[], unsigned int t){ // verificar se já não está ordenado
-    int i, j, min, aux;
-    if(ordenado(a,t) == false){
-        for (int i = 0 ; i < (t-1) ; i++){
-            min = i;
-            for (j = (i+1); j < t ; j++){
-                if(a[j] < a[min])
-                    min=j;
+/* Big(o) ==> O(n²)*/
+void selecao(int a[], unsigned int t){ 
+    int i, j, aux;
+    if(ordenado(a,t) == false){ // verificar se já não está ordenado    
+        for (i = 1 ; i < t ; i++){
+            aux = a[i];
+            for(j = i-1; j >= 0 && a[j] > aux ; j--){
+                    a[j+1] = a[j];
             }
-            if (a[i] != a[min]){
-                aux = a [i];
-                a[min] = aux;
-            }
+            a[j + 1] = aux;
         }
     } 
 }
-
-/* TODO: Implementar função */
+/* Big(o) ==>  O(n²)*/
 void insercao(int a[], unsigned int t){
-    int i, x;
+    unsigned int i, min, aux, k;
     if(ordenado(a,t) == false){ // verificar se já não está ordenado
         for (i = 0 ; i < t ; i++){
-            x = a[i];
-
+            min = i;
+            for(k = 0 ; k < i ; k++){
+                if(a[k] < min){
+                    min = a[k];
+                    aux = a[i];
+                }
+            }
+            a[i] = min;
+            a[k] = aux;
         }
     }
 }
 
-/* TODO: Implementar função */
+/* Big(o) ==> O(logn)*/
 void merge_sort(int a[], unsigned int t, int primeiro, int ultimo){
     int meio;
-    if(primeiro < ultimo){
+    if(!ordenado(a,t)){
+        if(primeiro < ultimo){
         meio = (primeiro + ultimo) / 2;
         merge_sort(a,t, primeiro, meio);
         merge_sort(a, t, meio+1 , ultimo);
         merge(a, primeiro, meio, meio+1, ultimo);
+        }
     }
+} /* Big(o) ==> O(n)*/
+void merge(int a[], int primeiro, int meio, int meioum, int ultimo) {
+    int* temp = new int[((meio - primeiro) + (ultimo - meioum) + 2)];
+    int i, j, k;
+    i = primeiro;
+    j = meioum;
+    k = 0;
+    while (i <= meio && j <= ultimo) {
+        if (a[i] < a[j])
+        temp[k++] = a[i++];
+        else
+        temp[k++] = a[j++];
+    }
+    while (i <= meio)
+        temp[k++] = a[i++];
+    while (j <= ultimo)
+        temp[k++] = a[j++];
+    for (i = primeiro, j = 0;i <= ultimo;i++, j++)
+        a[i] = temp[j];
+    delete[] temp;
 }
-void merge(int a[], int primeiro, int metade, int meio,int ultimo) {
-    
-}
+
