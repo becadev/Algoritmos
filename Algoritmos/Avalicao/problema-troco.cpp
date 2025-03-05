5316bf3a1db8993821c8214028fe47a901f3324f
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-03-05 05:02:34 +0000

    finish 1, amo

--- Algoritmos/Avalicao/problema-troco.cpp
+++ Algoritmos/Avalicao/problema-troco.cpp
@@ -16,18 +16,32 @@ int INF = 1000000;
 //     return dp[troco] = ans;
 // }
 
-int troco_pd(std::vector<int> &moedas, int troco){ // Programação dinâmica (Bottom-up)
-    dp[0] = 0; 
-    for(int i = 1 ; i <= troco ; i++){ // colunas da tabela (valores)
-        for(int k = 0; i < moedas.size() ; i++){// linhas da tabela (moedas)
-            if(i - k >= 0){ // vai preencher
-                dp[i] = min(dp[i], 1 + dp[i-k]); // min(INF, qtd moeda)
-            }
-        }
+int minimo_moedas_troco(std::vector<int> &moedas, int troco){ // Recursão sem memorization
+    if(troco == 0) return 0;
+    if(troco < 0) return INF;
+    int ans = INF;
+    for(int i = moedas.size()-1 ; i >= 0 ; i--){ 
+        ans = min(ans, 1+troco_pd(moedas, troco - moedas[i]));
     }
-
-
+    return ans;
 }
+
+    // 0 1 2 3 4 5 6 7
+//  1  0   
+//  3  
+//  4
+//  5
+// int troco_pd(std::vector<int> &moedas, int troco){ // Programação dinâmica (Bottom-up)
+//     dp[0] = 0; 
+//     for(int i = 1 ; i <= troco ; i++){ // colunas da tabela (valores)
+//         for(int k = 0; k < moedas.size() ; k++){// linhas da tabela (moedas)
+//             if(i - moedas[k] >= 0){ // valor-moeda
+//                 dp[i] = min(dp[i], 1 + dp[i-moedas[k]]); // min(INF, qtd moeda)
+//             }
+//         }
+//     }
+//     return dp[troco];
+// }
 
 
 // int troco_gulo(std::vector<int> &moedas,int troco){ // Método guloso
