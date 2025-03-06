5f793ccb173ca5c55206f392a236719837297c51
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-03-06 21:34:30 +0000

    .

---
+++ Algoritmos/Avaliacao/problema-moeda.cpp
@@ -0,0 +1,12 @@
+/*
+Um sistema monetário é composto de C moedas, cada uma com um valor Ci. Dado um valor V, determine de quantas formas distintas e ordenadas é possível obter o valor V. Exemplo: Um sistema com 3 moedas C = {2, 3, 5} possui 3 formas diferentes de obter o valor 9.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+
+    return 0;
+}
